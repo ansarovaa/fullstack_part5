ead67782a1dfa6ad55bd82f45d5a46320532cb8d
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-15 11:27:10 +0600

    task 5.6 and add style for 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,37 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+const Blog = ({blog}) => {
+    const blogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5
+    }
+    const [isVisible,
+        setVisible] = useState(false);
+
+    const handleToggle = () => {
+        setVisible(!isVisible);
+    }
+
+    return (
+
+        <div style ={blogStyle}>
+            {blog.title}
+            {blog.author}
+            <button onClick={handleToggle} className='btn ml-8'>
+                {isVisible
+                    ? 'Hide'
+                    : 'View'}
+            </button>
+            {isVisible && ( <> <p>{blog.url}</p> < p > Likes {blog.likes} < button > Like < /button> </p> <p>
+                by
+
+            </p> </>
+      )}
+        </div>
+
+    )
+}
 
 export default Blog
