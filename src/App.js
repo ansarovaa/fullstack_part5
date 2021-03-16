9d17ac7f47926b04e990a8824dd7b27653caf881
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-16 09:01:19 +0600

    task 5.9

--- src/App.js
+++ src/App.js
@@ -192,7 +192,7 @@ const App = () => {
                     </p>
 
                     <h2>blogs</h2>
-                    {blogs.map(blog => <Blog key={blog.id} blog={blog} onLikeClick={handleBlogLikeClick}/>)}
+                    {blogs.sort((first, second) => second.likes - first.likes).map(blog => <Blog key={blog.id} blog={blog} onLikeClick={handleBlogLikeClick}/>)}
                     <Togglable buttonLabel="Show Add blog form">
                         <AddBlog
                             addBlog={addBlog}
