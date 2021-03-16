2e3694b4c30b83ec672295c2d877bc889970522b
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-16 22:34:19 +0600

    task 5.12

--- src/components/AddBlog.js
+++ src/components/AddBlog.js
@@ -1,14 +1,14 @@
 import React from 'react'
-const AddBlog = ({addBlog, newTitle, newAuthor, newUrl, newLikes, handleAuthorChange, handleLikesChange, handleTitleChange, handleUrlChange}) => {
-    return (
-        <form onSubmit={addBlog}>
-            <div>title<input value={newTitle} onChange={handleTitleChange}/></div>
-            <div>author<input value={newAuthor} onChange={handleAuthorChange}/></div>
-            <div>url<input value={newUrl} onChange={handleUrlChange}/></div>
-            <div>likes<input value={newLikes} onChange={handleLikesChange}/></div>
-            <button type="submit">save</button>
-        </form>
-    )
+const AddBlog = ({ addBlog, newTitle, newAuthor, newUrl, newLikes, handleAuthorChange, handleLikesChange, handleTitleChange, handleUrlChange }) => {
+  return (
+    <form onSubmit={addBlog}>
+      <div>title<input value={newTitle} onChange={handleTitleChange}/></div>
+      <div>author<input value={newAuthor} onChange={handleAuthorChange}/></div>
+      <div>url<input value={newUrl} onChange={handleUrlChange}/></div>
+      <div>likes<input value={newLikes} onChange={handleLikesChange}/></div>
+      <button type="submit">save</button>
+    </form>
+  )
 
 }
 
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,48 +1,48 @@
-import React, {useState} from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const Blog = ({blog, onLikeClick, authUser, onRemoveClick, user}) => {
-    const blogStyle = {
-        paddingTop: 10,
-        paddingLeft: 2,
-        border: 'solid',
-        borderWidth: 1,
-        marginBottom: 5
-    }
-    
-    const [isVisible,
-        setVisible] = useState(false);
+const Blog = ({ blog, onLikeClick, authUser, onRemoveClick, user }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [isVisible,
+    setVisible] = useState(false)
 
-    const handleToggle = () => {
-        setVisible(!isVisible);
-    }
+  const handleToggle = () => {
+    setVisible(!isVisible)
+  }
 
-    Blog.propTypes = {
-        buttonLabel: PropTypes.string.isRequired
-      }
+  Blog.propTypes = {
+    buttonLabel: PropTypes.string.isRequired
+  }
 
 
-    return (
+  return (
 
-        <div style ={blogStyle}>
-            {blog.title}
-            {blog.author}
-            <button onClick={handleToggle} className='btn ml-8'>
-                {isVisible
-                    ? 'Hide'
-                    : 'View'}
-            </button>
-            {isVisible && ( <> <p>{blog.url}</p> < p > Likes {blog.likes} < button onClick = {onLikeClick.bind(null, blog.id)} className = 'btn ml-8'>Like</button> by blog.user.name</p></>)}
-            {authUser.username === blog.user.username && (
-            <button onClick={onRemoveClick.bind(
-                null,
-                blog.id,
-                blog.title,
-                blog.author
-              )} className='btn mt-12 blog-item-remove-btn'>Remove</button>)}
-        </div>
+    <div style ={blogStyle}>
+      {blog.title}
+      {blog.author}
+      <button onClick={handleToggle} className='btn ml-8'>
+        {isVisible
+          ? 'Hide'
+          : 'View'}
+      </button>
+      {isVisible && ( <> <p>{blog.url}</p> < p > Likes {blog.likes} < button onClick = {onLikeClick.bind(null, blog.id)} className = 'btn ml-8'>Like</button> by blog.user.name</p></>)}
+      {authUser.username === blog.user.username && (
+        <button onClick={onRemoveClick.bind(
+          null,
+          blog.id,
+          blog.title,
+          blog.author
+        )} className='btn mt-12 blog-item-remove-btn'>Remove</button>)}
+    </div>
 
-    )
+  )
 }
 
 export default Blog
