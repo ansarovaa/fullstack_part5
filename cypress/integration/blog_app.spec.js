59fcb70db1283002fc99e0f099eba9e2ee8a45d4
Anar Ansarova <Anar_Ansarova@epam.com>
2021-03-20 11:59:37 +0600

    task 5.17

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
-  /*  cy.request('POST', 'http://localhost:3000/api/testing/reset')*/
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
